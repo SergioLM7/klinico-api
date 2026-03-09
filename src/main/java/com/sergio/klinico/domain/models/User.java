469b09d0b9ceeb99743048d5c4d365022913991e
Sergio Lillo <sergiolillom@gmail.com>
2026-03-09 22:24:40 +0100

    UserDetails implemented in User domain model to be able to use getAuthorities() method

--- src/main/java/com/sergio/klinico/domain/models/User.java
+++ src/main/java/com/sergio/klinico/domain/models/User.java
@@ -3,12 +3,17 @@ package com.sergio.klinico.domain.models;
 import com.sergio.klinico.domain.models.enums.UserRole;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @Builder
-public class User {
+public class User implements UserDetails {
     private UUID id;
     private String email;
     private String password;
@@ -16,4 +21,16 @@ public class User {
     private String surname;
     private UserRole role;
     private boolean active;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override public boolean isEnabled() { return active; }
 }
