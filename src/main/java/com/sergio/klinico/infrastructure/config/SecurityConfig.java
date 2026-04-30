71dac6752cebdcb5c808a47ee44f6481c632fb6d
Sergio Lillo <sergiolillom@gmail.com>
2026-04-30 20:45:41 +0200

    HomeController added to redirect to swagger-ui

--- src/main/java/com/sergio/klinico/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/sergio/klinico/infrastructure/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable) // Desactivamos CSRF para APIs REST
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
